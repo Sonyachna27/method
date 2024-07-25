f15bd76142e3a269b420a614a28489c326ca1d6c
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-07-25 13:09:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const HMTLELEMENT = document.querySelector("html");
+document.addEventListener("DOMContentLoaded", function () {
+	openMobMenu();
+	killPreload();
+});
+
+//функція для завантаження контенту  ат прибирання прелоад
+
+const killPreload = () =>{
+
+	const preloadWrap = document.querySelector('.preload');
+	if(preloadWrap){
+		const preloadSwitch = document.querySelector('.preload-switch');
+		const animateLetters = document.querySelectorAll('.animate-logo svg:not([class])');
+		const animeCircle = document.querySelector('.anime-circle');
+		const animeCirclePath = animeCircle.querySelector('path');
+		preloadWrap.addEventListener('click', ()=>{
+			preloadWrap.style.animationName = 'removeOpacityPreload';
+		preloadSwitch.style.animationName = 'changeSwitchPadding';
+		function animateCharacters(){
+			animateLetters.forEach(ch => ch.style.animationName = 'animateLetters')
+			animeCirclePath.style.fill = "#00E600";
+		}
+		setTimeout(() => animateCharacters(), 100);
+		setTimeout(() => preloadWrap.style.display = "none", 1000);
+	});
+}
+}
+
+const openMobMenu = () =>{
+	const mobileMenu = document.querySelector(".header-nav");
+	const burger = document.querySelector('.burger');
+	if(burger){
+		burger.addEventListener('click', () =>{
+			HMTLELEMENT.classList.toggle('active');
+		})
+	}
+
+}
